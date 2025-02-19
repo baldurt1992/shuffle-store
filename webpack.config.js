94a0c4de05accbe5b23a54d5042dcc1456a4c22e
baldurt1992 <andresbaldur92@gmail.com>
2025-02-18 19:48:38 -0500

    chore: initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  mode: "development",
+  devtool: "source-map",
+  watch: true,
+};
